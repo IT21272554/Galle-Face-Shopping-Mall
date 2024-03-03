a078ed7713858d0c72dc3850d586eef9ab445ee0
Sathsara <sathsara2000@gmail.com>
2024-03-03 19:15:09 +0530

    Init

---
+++ backend/src/main/java/com/june_we_118/gallefaceshoppingmall/GallefaceshoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.june_we_118.gallefaceshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallefaceshoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallefaceshoppingmallApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/june_we_118/gallefaceshoppingmall/GallefaceshoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.june_we_118.gallefaceshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallefaceshoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

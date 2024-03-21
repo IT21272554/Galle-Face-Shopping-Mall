2d73b51c018adac5b907a831928c0376ddf8e270
Sathsara <sathsara2000@gmail.com>
2024-03-22 00:10:35 +0530

    search engine quary for items

--- backend/src/main/java/com/dev/gallefaceshoppingmall/controller/SearchEngineController.java
+++ backend/src/main/java/com/dev/gallefaceshoppingmall/controller/SearchEngineController.java
@@ -3,6 +3,8 @@ package com.dev.gallefaceshoppingmall.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +23,8 @@ public class SearchEngineController {
     SearchEngineService searchEngineService;
 
     @GetMapping("/query/{text}")
-    public List<Item> searchItemsOnly(@PathVariable String text){
-        return searchEngineService.searchItems(text);
+    public ResponseEntity<List<Item>> searchItemsOnly(@PathVariable String text){
+        List<Item> items = searchEngineService.searchItems(text);
+        return new ResponseEntity<>(items, HttpStatus.OK);
     }
 }
